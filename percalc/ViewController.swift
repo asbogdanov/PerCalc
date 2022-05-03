d8e6a950122c67faaa2cc8c4ff32e7253ee69568
Aleksandr Bogdanov <basik0204@gmail.com>
2022-05-03 21:25:55 +0300

    Added two more calculations and made clear coding

--- percalc/ViewController.swift
+++ percalc/ViewController.swift
@@ -7,17 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController {
+
+    var uiElements = ["Нахождение % от числа",
+                      "Сколько % составляет число от числа",
+                      "Прибавить % к числу"]
+
+    var pickerView = UIPickerView()
+
+    var selectedIndex: Int = -1
 
     @IBOutlet weak var TFPercent: UITextField!
     @IBOutlet weak var TFNumber: UITextField!
-
     @IBOutlet weak var labelResult: UILabel!
+    @IBOutlet weak var selectOperation: UITextField! {
+        didSet{
+            selectOperation.text = "Нажмите для выбора операции"
+            selectOperation.textAlignment = .center
+        }
+    }
+    @IBOutlet weak var firstLabel: UILabel! {
+        didSet{
+            firstLabel.isHidden = true
+        }
+    }
+    @IBOutlet weak var secondLabel: UILabel! {
+        didSet{
+            secondLabel.isHidden = true
+        }
+    }
+    @IBOutlet weak var percentLabel: UILabel! {
+        didSet{
+            percentLabel.isHidden = true
+        }
+    }
     @IBOutlet weak var cancelButton: UIButton! {
         didSet{
             cancelButton.layer.cornerRadius = 10
             cancelButton.frame.size = CGSize(width: 100, height: 60)
-
         }
     }
     @IBOutlet weak var okButton: UIButton! {
@@ -25,41 +52,155 @@ class ViewController: UIViewController, UITextFieldDelegate {
             okButton.layer.cornerRadius = 10
             okButton.frame.size = CGSize(width: 100, height: 60)
         }
-
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pickerView.delegate = self
+        pickerView.dataSource = self
+
+        selectOperation.inputView = pickerView
+        createToolBar()
+     }
+
 
     @IBAction func buttonOK(_ sender: UIButton) {
 
         if let percent = TFPercent.text, let number = TFNumber.text, percent.isEmpty || number.isEmpty {
 
-        let alert = UIAlertController(title: "Не заполнено поле", message: "Пожалуйста, заполните все поля.", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alert.addAction(okAction)
-        present(alert, animated: true, completion: nil)
-        } else { return labelResult.text = "\(TFPercent.text!)% от числа \(TFNumber.text!) = \((Double(TFNumber.text!)!) * 0.01 * (Double(TFPercent.text!)!))" }
+            // создание всплывающих алертов в случае пустых полей
+            let alert = UIAlertController(title: "Не заполнено поле", message: "Пожалуйста, заполните все поля.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+        } else {
+
+            let number = (Double(TFNumber.text!)!)
+            let percent = (Double(TFPercent.text!)!)
+
+            switch selectedIndex {
+            case 0:
+                let calcValue = calcPercentFromNumber(number: number, percent: percent)
+                labelResult.text = "\(TFPercent.text!)% от числа \(TFNumber.text!) = \(calcValue)"
+                break
+
+            case 1:
+                let calcValue = calcPercentNumberFromNumber(number: number, percent: percent)
+                labelResult.text = "Число \(TFPercent.text!) от числа \(TFNumber.text!) = \(calcValue)%"
+                break
+
+            case 2:
+                let calcValue = calcAddPercentToNumber(number: number, percent: percent)
+                labelResult.text = "\(TFNumber.text!) + \(TFPercent.text!)% = \(calcValue)%"
+                break
+
+            default:
+                break
+            }
+        }
+    }
+
+    func calcPercentFromNumber (number: Double, percent: Double) -> Double {
+        let result = number * 0.01 * percent
+        return result
+    }
+
+    func calcPercentNumberFromNumber (number: Double, percent: Double) -> Double {
+        let result = (percent / number) * 100
+        return result
+    }
+
+    func calcAddPercentToNumber (number: Double, percent: Double) -> Double {
+        let result = number * (1 + percent / 100)
+        return result
     }
 
     @IBAction func buttonCancel(_ sender: UIButton) {
         TFPercent.text = ""
         TFNumber.text = ""
         labelResult.text = "Здесь отобразится результат!"
+        selectOperation.text = "Нажмите для выбора операции"
+
+        view.endEditing(true) //убирает клавиатуру при нажатии кнопки "Сброс"
     }
 
+    // создание кнопки "Готово" на pickerView
+    func createToolBar() {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+
+        let doneButton = UIBarButtonItem(title: "Готово",
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(dismissKeyboard))
+
+        toolbar.setItems([doneButton], animated: true)
+        toolbar.isUserInteractionEnabled = true
+
+        selectOperation.inputAccessoryView = toolbar
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+
+    // Метод скрытия клавиатуры при нажатии на любое место на экране
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
 
-     }
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
 
-    override func canPerformAction(
-        _ action: Selector, withSender sender: Any?) -> Bool {
-          return super.canPerformAction(action, withSender: sender)
-          && (action == #selector(UIResponderStandardEditActions.cut)
-          || action == #selector(UIResponderStandardEditActions.copy))
-      }
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return uiElements.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return uiElements[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+
+        selectOperation.text = uiElements[row]
+        selectedIndex = row
+
+        switch row {
+        case 0:
+            percentLabel.isHidden = false
+            firstLabel.isHidden = false
+            secondLabel.isHidden = false
+
+            firstLabel.text = "Сколько составляет"
+            secondLabel.text = "от числа"
+
+            break
+
+        case 1:
+            percentLabel.isHidden = true
+            firstLabel.isHidden = false
+            secondLabel.isHidden = false
+
+            firstLabel.text = "Сколько составляет"
+            secondLabel.text = "от числа"
+
+            break
+
+        case 2:
+            percentLabel.isHidden = false
+            firstLabel.text = "Прибавить"
+            secondLabel.text = "к числу"
+
+            break
+
+        default:
+            break
+        }
+    }
 }
 
 
